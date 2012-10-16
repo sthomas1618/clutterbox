504b6b9e0517974851c0c67cb1a1360344510eee
Stephen Thomas <sthomas1618@gmail.com>
2012-10-15 22:17:42 -0700

    Add Users and Bookmarks controllers. Add custom.css. Add routing for root, users, and bookmarsk. Add Bookmarks#index

--- bookmarks_app/config/routes.rb
+++ bookmarks_app/config/routes.rb
@@ -1,4 +1,9 @@
 BookmarksApp::Application.routes.draw do
+
+  resources :bookmarks
+  resources :users
+  root to: 'bookmarks#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
