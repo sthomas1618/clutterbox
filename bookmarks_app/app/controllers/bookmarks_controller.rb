537d1a7d80a6bee4260c945b5f77eedd8d7c37b9
Stephen Thomas <sthomas1618@gmail.com>
2012-10-16 15:54:43 -0700

    Add bookmark show. Fix header links. Finish project3

--- bookmarks_app/app/controllers/bookmarks_controller.rb
+++ bookmarks_app/app/controllers/bookmarks_controller.rb
@@ -5,7 +5,8 @@ class BookmarksController < ApplicationController
   end
 
   def show 
-    @bookmarks = all_bookmarks[params[:id].to_i]
+    @bookmark = all_bookmarks[params[:id].to_i]
+    @index = params[:id]
   end
 
   def new
@@ -27,7 +28,7 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    flash[:success] = "Removed Bookmark."
+    flash[:success] = "Removed Bookmark"
     redirect_to user_path(1)
   end
 
@@ -38,28 +39,32 @@ class BookmarksController < ApplicationController
         domain:      "yahoo.com",
         title:       "I don't know why I bookmarked this",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        raiting:     1
+        rating:      1,
+        tags:        "News, Kickstarter, Startups, Technology"
       },
       {
         url:         "http://www.github.com",
         domain:      "github.com",
         title:       "My favorite git website",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        raiting:     4
+        rating:      4,
+        tags:        "News, Kickstarter, Startups, Technology"
       },
       {
         url:         "http://www.google.com",
         domain:      "google.com",
         title:       "Google: a wonderful search engine",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        raiting:     5
+        rating:      5,
+        tags:        "News, Kickstarter, Startups, Technology"
       },
       {
         url:         "http://blogs.reuters.com/felix-salmon/2012/09/18/kickstarter-vaporware-of-the-day-lifx-edition/",
         domain:      "blogs.reuters.com",
         title:       "Some article on kickstarter",
         description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        raiting:     3
+        rating:      3,
+        tags:       "News, Kickstarter, Startups, Technology"
       }
     ]
   end
--- bookmarks_app/app/controllers/users_controller.rb
+++ bookmarks_app/app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    flash[:success] = "Removed user."
+    flash[:success] = "Removed user"
     redirect_to users_path
   end
 
@@ -46,28 +46,28 @@ class UsersController < ApplicationController
             domain:      "yahoo.com",
             title:       "I don't know why I bookmarked this",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     1
+            rating:     1
           },
           {
             url:         "http://www.github.com",
             domain:      "github.com",
             title:       "My favorite git website",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     4
+            rating:     4
           },
           {
             url:         "http://www.google.com",
             domain:      "google.com",
             title:       "Google: a wonderful search engine",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     5
+            rating:     5
           },
           {
             url:         "http://blogs.reuters.com/felix-salmon/2012/09/18/kickstarter-vaporware-of-the-day-lifx-edition/",
             domain:      "blogs.reuters.com",
             title:       "Some article on kickstarter",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     3
+            rating:     3
           },
         ]
       },
@@ -84,28 +84,28 @@ class UsersController < ApplicationController
             domain:      "yahoo.com",
             title:       "I don't know why I bookmarked this",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     1
+            rating:     1
           },
           {
             url:         "http://www.github.com",
             domain:      "github.com",
             title:       "My favorite git website",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     4
+            rating:     4
           },
           {
             url:         "http://www.google.com",
             domain:      "google.com",
             title:       "Google: a wonderful search engine",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     5
+            rating:     5
           },
           {
             url:         "http://blogs.reuters.com/felix-salmon/2012/09/18/kickstarter-vaporware-of-the-day-lifx-edition/",
             domain:      "blogs.reuters.com",
             title:       "Some article on kickstarter",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     3
+            rating:     3
           },
         ]
       },
@@ -122,28 +122,28 @@ class UsersController < ApplicationController
             domain:      "yahoo.com",
             title:       "I don't know why I bookmarked this",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     1
+            rating:     1
           },
           {
             url:         "http://www.github.com",
             domain:      "github.com",
             title:       "My favorite git website",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     4
+            rating:     4
           },
           {
             url:         "http://www.google.com",
             domain:      "google.com",
             title:       "Google: a wonderful search engine",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     5
+            rating:     5
           },
           {
             url:         "http://blogs.reuters.com/felix-salmon/2012/09/18/kickstarter-vaporware-of-the-day-lifx-edition/",
             domain:      "blogs.reuters.com",
             title:       "Some article on kickstarter",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            raiting:     3
+            rating:     3
           },
         ]
       }
