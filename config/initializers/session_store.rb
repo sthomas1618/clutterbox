356666a3da71836eaa1e76959d025e66544f0235
Stephen Thomas <sthomas1618@gmail.com>
2013-03-08 22:47:04 -0800

    oops, forgot this

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Clutterbox::Application.config.session_store :cookie_store, key: '_bookmarks_app_session'
+Clutterbox::Application.config.session_store :cookie_store, key: '_clutterbox_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# BookmarksApp::Application.config.session_store :active_record_store
+# Clutterbox::Application.config.session_store :active_record_store
